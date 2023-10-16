7090de097c8136dcdb23fa17ad1bc168651e5642
CodeWithAkbarov <codewithakbarov@gmail.com>
2023-10-16 17:51:13 +0500

    added toast function

--- app/src/main/java/com/tutacademy/codereviewapplication/MainActivity.kt
+++ app/src/main/java/com/tutacademy/codereviewapplication/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showToast()
+    }
 
+    private fun showToast() {
+        Toast.makeText(this, "Just toast", Toast.LENGTH_SHORT).show()
     }
 }
