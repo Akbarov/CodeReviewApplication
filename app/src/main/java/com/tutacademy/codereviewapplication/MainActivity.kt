fb580fe999b6fb9b1e13995d4757e6a2ef923bf5
Akbarov <zohidjonakbarov@gmail.com>
2023-10-16 15:26:36 +0500

    show toast function added

--- app/src/main/java/com/tutacademy/codereviewapplication/MainActivity.kt
+++ app/src/main/java/com/tutacademy/codereviewapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.tutacademy.codereviewapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showToast()
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, "Just Toast", Toast.LENGTH_SHORT).show()
     }
 }
